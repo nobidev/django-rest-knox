ba066619df6bbaa9200ce5279129bdb8b6c0b642
Nobi <nobi@nobidev.com>
2024-06-12 09:34:18 +0700

    Store data and pass context to create method for AuthToken

--- knox/models.py
+++ knox/models.py
@@ -9,28 +9,28 @@ User = knox_settings.USER_MODEL or settings.AUTH_USER_MODEL
 
 
 class AuthTokenManager(models.Manager):
-    def create(self, user, expiry=knox_settings.TOKEN_TTL):
+    def create(self, user, expiry=knox_settings.TOKEN_TTL, context=None, **kwargs):
         token = crypto.create_token_string()
         digest = crypto.hash_token(token)
 
         if expiry is not None:
             expiry = timezone.now() + expiry
 
-        instance = super(AuthTokenManager, self).create(
-            token_key=token[:CONSTANTS.TOKEN_KEY_LENGTH], digest=digest,
-            user=user, expiry=expiry)
+        data = None
+        if context and 'knox_data' in context:
+            data = context['knox_data']
+
+        instance = super(AuthTokenManager, self).create(token_key=token[:CONSTANTS.TOKEN_KEY_LENGTH], digest=digest, user=user, expiry=expiry, data=data)
         return instance, token
 
 
 class AuthToken(models.Model):
     objects = AuthTokenManager()
 
-    digest = models.CharField(
-        max_length=CONSTANTS.DIGEST_LENGTH, primary_key=True)
-    token_key = models.CharField(
-        max_length=CONSTANTS.TOKEN_KEY_LENGTH, db_index=True)
-    user = models.ForeignKey(User, null=False, blank=False,
-                             related_name='auth_token_set', on_delete=models.CASCADE)
+    digest = models.CharField(max_length=CONSTANTS.DIGEST_LENGTH, primary_key=True)
+    token_key = models.CharField(max_length=CONSTANTS.TOKEN_KEY_LENGTH, db_index=True)
+    user = models.ForeignKey(User, null=False, blank=False, related_name='auth_token_set', on_delete=models.CASCADE)
+    data = models.JSONField(null=True)
     created = models.DateTimeField(auto_now_add=True)
     expiry = models.DateTimeField(null=True, blank=True)
 
--- knox/views.py
+++ knox/views.py
@@ -60,7 +60,7 @@ class LoginView(APIView):
                     status=status.HTTP_403_FORBIDDEN
                 )
         token_ttl = self.get_token_ttl()
-        instance, token = AuthToken.objects.create(request.user, token_ttl)
+        instance, token = AuthToken.objects.create(request.user, token_ttl, context=self.get_context())
         user_logged_in.send(sender=request.user.__class__,
                             request=request, user=request.user)
         data = self.get_post_response_data(request, token, instance)
